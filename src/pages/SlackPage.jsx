47a0e5d0f6808c0475290fbf4a5adf9e860b076c
Junelle Puzon <nellepuzon@gmail.com>
2022-04-15 19:08:41 +0800

    slack page

--- src/pages/SlackPage.jsx
+++ src/pages/SlackPage.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Main from '../components/slack-page/Main';
 import NavBar from '../components/slack-page/NavBar';
+import SideBar from '../components/slack-page/SideBar';
 
 function SlackPage() {
-  return <NavBar />;
+  return (
+    <div>
+      <div className='slack-navbar'>
+        <NavBar />
+      </div>
+      <div className='slack-main'>
+        <SideBar/>
+        <Outlet/>
+      </div>
+    </div>
+  );
 }
 
 export default SlackPage;
